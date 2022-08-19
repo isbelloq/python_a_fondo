ca422fadad70b94fad0ad193051ef4bf8b43ed48
Santiago Bello <isbelloq@gmail.com>
2022-08-19 16:11:47 -0500

    Se agrega el material del primer capitulo y una parte parcial del segundo capitulo

---
+++ cap1-introduccion_al_lenguaje_python/ejemplo_simple.py
@@ -0,0 +1,22 @@
+import math
+
+
+def hola_mundo():
+    print('Hola Mundo')
+
+
+def calcular_raiz_cuadrada(num):
+    return math.sqrt(num)
+
+
+def funcion_principal(longitud):
+    hola_mundo()
+    print([x * 2 for x in range(longitud)])
+    while True:
+        num = float(input('Introduzca un numero: '))
+        res = calcular_raiz_cuadrada(num)
+        print(f'La raiz cuadrada de {num} es {res}')
+
+
+if __name__ == '__main__':
+    funcion_principal(10)
